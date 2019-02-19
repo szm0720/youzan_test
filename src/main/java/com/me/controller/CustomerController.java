7e0c4e0f2c09c6ec7c5dd6664fd9fec083555c71
shishimei <shimingming@yunniao.me>
2019-02-19 14:08:10 +0800

    2019年02月19日14:08:04

--- src/main/java/com/me/controller/CustomerController.java
+++ src/main/java/com/me/controller/CustomerController.java
@@ -25,11 +25,11 @@ public class CustomerController {
         log.error("str的值是:{}", str);
 
         JSONObject jsonObject1 = JSON.parseObject(str);
-        log.error("转换{}",jsonObject1.toJSONString());
+        log.error("转换{}", jsonObject1.toJSONString());
 
         JSONObject jsonObject = new JSONObject();
-        jsonObject.put("code",0);
-        jsonObject.put("msg","success");
+        jsonObject.put("code", 0);
+        jsonObject.put("msg", "success");
 
         return jsonObject.toJSONString();
 
@@ -41,7 +41,7 @@ public class CustomerController {
         SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
 
         String format = simpleDateFormat.format(new Date());
-
+        log.info(format);
         return format;
 
     }
